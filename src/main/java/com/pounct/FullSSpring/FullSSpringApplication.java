1eaa93a5d158b743ede6bb9dcebae6564ff3d5a1
Pounct <53088375+pounct@users.noreply.github.com>
2024-08-23 22:40:17 +0200

    Initialisation...

---
+++ src/main/java/com/pounct/FullSSpring/FullSSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pounct.FullSSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pounct/FullSSpring/FullSSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pounct.FullSSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
