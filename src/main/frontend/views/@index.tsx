6278efa1829e9e3433aba6269f2b1ed5df3b087a
Pounct <53088375+pounct@users.noreply.github.com>
2024-08-25 20:05:49 +0200

    add styles and  initial view of a list mots...

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "../styles/styles.css";
+        import MotComponent from "../components/mot-component";
 
         export default function Index() {
         return (
-<div>
-    <h1>Hello Vaadin</h1>
+<div className="p-m">
+    <MotComponent/>
 </div>
         );
         }
---
+++ src/main/java/com/pounct/FullSSpring/services/package-info.java
@@ -0,0 +1,5 @@
+
+@NonNullApi
+package com.pounct.FullSSpring.services;
+
+import reactor.util.annotation.NonNullApi;
